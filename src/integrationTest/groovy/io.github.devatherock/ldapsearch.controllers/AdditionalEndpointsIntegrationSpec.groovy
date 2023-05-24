5532eaca2e06ef38171c39313abacadb5b8196f1
devatherock <devatherock@gmail.com>
2023-05-24 18:28:45 -0500

    test: Added tests for built-in endpoints like /health

---
+++ src/integrationTest/groovy/io.github.devatherock/ldapsearch.controllers/AdditionalEndpointsIntegrationSpec.groovy
@@ -0,0 +1,10 @@
+package io.github.devatherock.ldapsearch.controllers
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+
+/**
+ * Integration test for additional endpoints
+ */
+@MicronautTest(propertySources = 'classpath:application-integration.yml', startApplication = false)
+class AdditionalEndpointsIntegrationSpec extends AdditionalEndpointsSpec {
+}
